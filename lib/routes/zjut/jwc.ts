d53d915623b8212adfd79bed171d996d392d1d10
kar1n1911 <114369000+kar1n1911@users.noreply.github.com>
2024-08-30 22:27:39 +0800

    Create jwc.ts

---
+++ lib/routes/zjut/jwc.ts
@@ -0,0 +1,86 @@
+import { Route } from '@/types';
+import { load } from 'cheerio';
+import cache from '@/utils/cache';
+import got from '@/utils/got';
+import { parseDate } from '@/utils/parse-date';
+
+const link = 'http://www.jwc.zjut.edu.cn';
+const host = 'http://www.jwc.zjut.edu.cn/1849/list.htm';
+
+const getSingleRecord = async () => {
+    const res = await got(host);
+
+    const $ = load(res.data);
+    const list = $('.list2').find('li');
+
+    return (
+        list &&
+        list
+            .map((index, item) => {
+                item = $(item);
+                const dateTxt = item.find('.news_meta').text();
+                const link_txt = item.find('a').attr('href');
+                const title_txt = item.find('a').attr('title');
+                return link_txt.startsWith('http')
+                    ? {
+                          title: title_txt,
+                          pubDate: parseDate(dateTxt),
+                          link: 'http://www.jwc.zjut.edu.cn/2024/0621/c1840a268758/page.htm',
+                      }
+                    : {
+                          title: title_txt,
+                          pubDate: parseDate(dateTxt),
+                          link: link + link_txt,
+                      };
+            })
+            .get()
+    );
+};
+
+export const route: Route = {
+    path: '/jwc',
+    categories: ['university'],
+    example: '/zjut/jwc',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    name: '浙工大教务处 教学通知',
+    maintainers: ['kar1n1911'],
+    handler,
+    url: 'www.jwc.zjut.edu.cn/1849/list.htm',
+    radar: [
+        {
+            source: ['www.jwc.zjut.edu.cn/1849/list.htm'],
+        },
+    ],
+};
+
+async function handler() {
+    const items = await getSingleRecord();
+    const out = await Promise.all(
+        items.map((item) =>
+            cache.tryGet(item.link, async () => {
+                const response = await got(item.link);
+                const $ = load(response.data);
+                return {
+                    title: item.title,
+                    link: item.link,
+                    description: $('.wp_articlecontent').html(),
+                    pubDate: item.pubDate,
+                };
+            })
+        )
+    );
+    return {
+        title: '浙工大教务处 教学通知',
+        description: '浙江工业大学-通知公告',
+        link: host,
+        item: out,
+    };
+}
